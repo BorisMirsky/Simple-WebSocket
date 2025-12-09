21f7cc0199df66c3c68952d4f6b4584d5d9a2307
Boris Mirsky <boris.y.mirsky@gmail.com>
2025-12-09 22:33:36 +0300

    first commit

---
+++ src/client.py
@@ -0,0 +1,40 @@
+
+import random
+import websockets
+import asyncio
+from faker import Faker
+from faker.providers.person.ru_RU import Provider
+
+# создание объекта Faker с локализацией для России
+fake = Faker('ru_RU')
+fake.add_provider(Provider)
+
+# асинхронная функция получения сообщения от сервера
+async def receive_message(websocket):
+    try:
+        async for message in websocket:
+            print(f"Получено сообщение от сервера: {message}")
+    except websockets.ConnectionClosed:
+        print("Соединение закрыто")
+
+# асинхронная функция отправки сообщения на сервер
+async def send_message():
+    uri = "ws://localhost:1000"
+    async with websockets.connect(uri) as websocket:
+        # Запуск корутины для получения сообщений от сервера
+        receive_task = asyncio.create_task(receive_message(websocket))
+
+        try:
+            while True:
+                sec = random.randint(1, 10)
+                message = fake.name()
+                await websocket.send(message)
+                print(f"Отправлено сообщение серверу от клиента: У нас новый пользователь {message}, сгенери ему емейл")
+                await asyncio.sleep(sec)  # Ожидание перед отправкой следующего сообщения
+        except websockets.ConnectionClosed:
+            print("Соединение закрыто")
+        finally:
+            receive_task.cancel()
+
+# Запуск корутины send_message() с учетом уже запущенного цикла событий
+asyncio.get_event_loop().run_until_complete(send_message())
---
+++ src/server.py
@@ -0,0 +1,37 @@
+
+import asyncio
+import websockets
+from faker import Faker                            # библиотекa генерации фейковых данных 
+from faker.providers.person.ru_RU import Provider
+import random
+
+#назначаем порт на локалхосте
+port = 1000
+
+# создание объекта Faker с локализацией для России
+fake = Faker('ru_RU')
+fake.add_provider(Provider)
+
+# асинхронная функция
+async def handler(websocket, path):
+    try:
+        async for message in websocket:
+            op = random.randint(0, 100)
+            print(f"Получено сообщение от клиента - новому пользователю {message} нужен email")
+            if op>10 :
+             message_ser = fake.free_email()
+             print(f"Отправлено клиенту для пользователя {message} сгенерирован email {message_ser}")
+            else:
+                message_ser = "сервер перегружен"
+                print(f"Для пользователя {message} не удалось сгенерировать email, т.к. {message_ser}")
+            await websocket.send(f"{message_ser}")
+    except websockets.ConnectionClosed:
+        print("Соединение закрыто")
+
+# объявляем сервер
+start_server = websockets.serve(handler, "localhost", port)
+
+# запускаем сервер
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
+
