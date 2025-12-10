05d4955f97dc7452d403c3b1fc3d325b4e281e61
Boris Mirsky <boris.y.mirsky@gmail.com>
2025-12-10 21:25:24 +0300

    in process

--- src/server.py
+++ src/server.py
@@ -18,9 +18,9 @@ async def handler(websocket, path):
         async for message in websocket:
             op = random.randint(0, 100)
             print(f"Получено сообщение от клиента - новому пользователю {message} нужен email")
-            if op>10 :
-             message_ser = fake.free_email()
-             print(f"Отправлено клиенту для пользователя {message} сгенерирован email {message_ser}")
+            if op > 10 :
+                message_ser = fake.free_email()
+                print(f"Отправлено клиенту для пользователя {message} сгенерирован email {message_ser}")
             else:
                 message_ser = "сервер перегружен"
                 print(f"Для пользователя {message} не удалось сгенерировать email, т.к. {message_ser}")
@@ -35,3 +35,54 @@ start_server = websockets.serve(handler, "localhost", port)
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
 
+
+
+
+"""
+async def handler(websocket):
+    while True:
+        try:
+            message = await websocket.recv()
+            print('Message received from client: ', message)
+            await websocket.send("Message from server: " + message)
+        except Exception as e:
+            print(e)
+            break
+
+async def main():
+    async with websockets.serve(handler, "", 8001):  # listen at port 8001
+        await asyncio.Future()                       # run forever
+
+if __name__ == "__main__":
+    asyncio.run(main())
+
+#############################
+
+
+connected_clients = set()
+
+async def handle_client(websocket, path):
+    # Add the new client to the set of connected clients
+    connected_clients.add(websocket)
+    try:
+        # Listen for messages from the client
+        async for message in websocket:
+            # Broadcast the message to all other connected clients
+            for client in connected_clients:
+                if client != websocket:
+                    await client.send(message)
+    except websockets.exceptions.ConnectionClosed:
+        pass
+    finally:
+        # Remove the client from the set of connected clients
+        connected_clients.remove(websocket)
+
+async def main():
+    server = await websockets.serve(handle_client, 'localhost', 12345)
+    await server.wait_closed()
+
+if __name__ == "__main__":
+    asyncio.run(main())
+
+
+"""
